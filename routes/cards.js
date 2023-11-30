847b2a187a0042f730c0199ecd71e2fd77668831
Anastasia Odintsova <Barsurku@yandex.ru>
2023-11-30 21:49:59 +0300

    ПР13 t fixes 3

--- routes/cards.js
+++ routes/cards.js
@@ -3,10 +3,10 @@ const {
   createCard, getCards, likeCard, dislikeCard, deleteCard,
 } = require('../controllers/cards');
 
-router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
-router.delete('/cards/:cardId/likes', deleteCard);
+router.get('/', getCards);
+router.post('/', createCard);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
+router.delete('/:cardId/likes', deleteCard);
 
 module.exports = router;
