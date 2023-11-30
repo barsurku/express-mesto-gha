6cc1d2a2e827ddc410261703fef57abe0d77e210
Anastasia Odintsova <Barsurku@yandex.ru>
2023-12-01 00:46:56 +0300

    sdif

--- app.js
+++ app.js
@@ -9,20 +9,19 @@ app.use(express.json());
 
 const { PORT = 3000 } = process.env;
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6568ff7ea8f9df2ef10e871c',
+  };
+
+  next();
+});
 app.use('/', usersRouter);
 app.use('/cards', cardsRouter);
 app.use('*', (req, res) => {
   res.status(notFound).send({
     message: 'Страница не найдена',
   });
-});
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6568efda30a81f8a535e08c8',
-  };
-
-  next();
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
