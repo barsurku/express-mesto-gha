aab8db165f61f94ca158db8992a60d3b14cf20ff
Anastasia Odintsova <Barsurku@yandex.ru>
2023-12-03 17:08:10 +0300

    и

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,7 @@ const NotFound = require('../utils/error/notFound');
 const BadRequest = require('../utils/error/badRequest');
 const Conflict = require('../utils/error/Conflict');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { JWT_SECRET } = process.env;
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
@@ -19,7 +19,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(() => {
