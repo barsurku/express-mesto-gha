eca4aefb367629a9d5bd5a1690b57ec8a3c06155
Anastasia Odintsova <Barsurku@yandex.ru>
2023-12-03 21:48:50 +0300

    Fin2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,10 +5,10 @@ const { JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
-  const token = authorization.replace('Bearer', '');
+  const token = authorization.replace('Bearer ', '');
 
   let payload;
   try {
