0eb883bc8b5114ca51f32748ff41ac3e2ee43723
Anastasia Odintsova <Barsurku@yandex.ru>
2023-12-04 16:47:04 +0300

    f

--- controllers/users.js
+++ controllers/users.js
@@ -19,7 +19,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((err) => next(err));
